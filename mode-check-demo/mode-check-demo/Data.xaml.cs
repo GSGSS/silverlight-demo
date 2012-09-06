46bf22710dd491e64f1d707b3f707d9d0a1eb9ee
kiwi <kiwi.swhite.coder@gmail.com>
2012-09-06 09:24:25 +0800

    mode-check-demo cannot display MainPage.xml in design mode

---
+++ mode-check-demo/mode-check-demo/Data.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using mode_check_demo.MyDataService;
+
+namespace mode_check_demo
+{
+    public partial class Data : UserControl
+    {
+        public Data()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var client = new MyDataServiceSoapClient();
+            client.GetDataCompleted += (s, args) => displayBox.Text = args.Result;
+            client.GetDataAsync();
+        }
+    }
+}
