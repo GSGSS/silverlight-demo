6819002c65197e4d906052cd5d189b9a3ba0580e
kiwi <kiwi.swhite.coder@gmail.com>
2012-10-22 12:23:52 +0800

    databinding-converter-demo convert double value to image source string

---
+++ databinding-converter-demo/databinding-converter-demo/ColorToPicture.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace databinding_converter_demo
+{
+    public class ColorToPicture : IValueConverter 
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double num = (double)value;
+            return num < 50 ? "Images/green.png" : "Images/red.png";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
