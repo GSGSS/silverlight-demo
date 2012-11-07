f430ea5587d02c0d3fbe969927a98f55ca484381
kiwi <kiwi.swhite.coder@gmail.com>
2012-11-07 16:46:09 +0800

    ria-association-domain-service show association entities

---
+++ ria-association-domain-service/ria-association-domain-service.Web/LibraryService.cs
@@ -0,0 +1,104 @@
+﻿
+namespace ria_association_domain_service.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.EntityFramework;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // Implements application logic using the LibraryEntities context.
+    // TODO: Add your application logic to these methods or in additional methods.
+    // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
+    // Also consider adding roles to restrict access as appropriate.
+    // [RequiresAuthentication]
+    [EnableClientAccess()]
+    public class LibraryService : LinqToEntitiesDomainService<LibraryEntities>
+    {
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Books' query.
+        public IQueryable<Book> GetBooks()
+        {
+            return this.ObjectContext.Books;
+        }
+
+        public void InsertBook(Book book)
+        {
+            if ((book.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(book, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.Books.AddObject(book);
+            }
+        }
+
+        public void UpdateBook(Book currentBook)
+        {
+            this.ObjectContext.Books.AttachAsModified(currentBook, this.ChangeSet.GetOriginal(currentBook));
+        }
+
+        public void DeleteBook(Book book)
+        {
+            if ((book.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(book, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.Books.Attach(book);
+                this.ObjectContext.Books.DeleteObject(book);
+            }
+        }
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Records' query.
+        public IQueryable<Record> GetRecords()
+        {
+            return this.ObjectContext.Records.Include("Book");
+        }
+
+        public void InsertRecord(Record record)
+        {
+            if ((record.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(record, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.Records.AddObject(record);
+            }
+        }
+
+        public void UpdateRecord(Record currentRecord)
+        {
+            this.ObjectContext.Records.AttachAsModified(currentRecord, this.ChangeSet.GetOriginal(currentRecord));
+        }
+
+        public void DeleteRecord(Record record)
+        {
+            if ((record.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(record, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.Records.Attach(record);
+                this.ObjectContext.Records.DeleteObject(record);
+            }
+        }
+    }
+}
+
+
---
+++ ria-association-domain-service/ria-association-domain-service.Web/LibraryService.metadata.cs
@@ -0,0 +1,82 @@
+﻿
+namespace ria_association_domain_service.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data.Objects.DataClasses;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // The MetadataTypeAttribute identifies BookMetadata as the class
+    // that carries additional metadata for the Book class.
+    [MetadataTypeAttribute(typeof(Book.BookMetadata))]
+    public partial class Book
+    {
+
+        // This class allows you to attach custom attributes to properties
+        // of the Book class.
+        //
+        // For example, the following marks the Xyz property as a
+        // required property and specifies the format for valid values:
+        //    [Required]
+        //    [RegularExpression("[A-Z][A-Za-z0-9]*")]
+        //    [StringLength(32)]
+        //    public string Xyz { get; set; }
+        internal sealed class BookMetadata
+        {
+
+            // Metadata classes are not meant to be instantiated.
+            private BookMetadata()
+            {
+            }
+
+            public int Id { get; set; }
+
+            public string Isbn { get; set; }
+
+            public string Name { get; set; }
+
+            public EntityCollection<Record> Records { get; set; }
+        }
+    }
+
+    // The MetadataTypeAttribute identifies RecordMetadata as the class
+    // that carries additional metadata for the Record class.
+    [MetadataTypeAttribute(typeof(Record.RecordMetadata))]
+    public partial class Record
+    {
+
+        // This class allows you to attach custom attributes to properties
+        // of the Record class.
+        //
+        // For example, the following marks the Xyz property as a
+        // required property and specifies the format for valid values:
+        //    [Required]
+        //    [RegularExpression("[A-Z][A-Za-z0-9]*")]
+        //    [StringLength(32)]
+        //    public string Xyz { get; set; }
+        internal sealed class RecordMetadata
+        {
+
+            // Metadata classes are not meant to be instantiated.
+            private RecordMetadata()
+            {
+            }
+
+            [Include]
+            public Book Book { get; set; }
+
+            public int bookId { get; set; }
+
+            public DateTime endTime { get; set; }
+
+            public int id { get; set; }
+
+            public DateTime startTime { get; set; }
+        }
+    }
+}
