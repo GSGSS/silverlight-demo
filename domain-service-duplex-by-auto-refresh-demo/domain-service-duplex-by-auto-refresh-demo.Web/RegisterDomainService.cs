73139ae66d4ff90b8eb1777b1bd4fd95f1ce2ac6
kiwi <kiwi.swhite.coder@gmail.com>
2012-11-28 13:12:58 +0800

    domain data source implement duplex by autorefreshing

---
+++ domain-service-duplex-by-auto-refresh-demo/domain-service-duplex-by-auto-refresh-demo.Web/RegisterDomainService.cs
@@ -0,0 +1,65 @@
+﻿
+namespace domain_service_duplex_by_auto_refresh_demo.Web
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Data;
+    using System.Linq;
+    using System.ServiceModel.DomainServices.EntityFramework;
+    using System.ServiceModel.DomainServices.Hosting;
+    using System.ServiceModel.DomainServices.Server;
+
+
+    // Implements application logic using the ChatEntities context.
+    // TODO: Add your application logic to these methods or in additional methods.
+    // TODO: Wire up authentication (Windows/ASP.NET Forms) and uncomment the following to disable anonymous access
+    // Also consider adding roles to restrict access as appropriate.
+    // [RequiresAuthentication]
+    [EnableClientAccess()]
+    public class RegisterDomainService : LinqToEntitiesDomainService<ChatEntities>
+    {
+
+        // TODO:
+        // Consider constraining the results of your query method.  If you need additional input you can
+        // add parameters to this method or create additional query methods with different names.
+        // To support paging you will need to add ordering to the 'Registers' query.
+        public IQueryable<Register> GetRegisters()
+        {
+            return this.ObjectContext.Registers;
+        }
+
+        public void InsertRegister(Register register)
+        {
+            if ((register.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(register, EntityState.Added);
+            }
+            else
+            {
+                this.ObjectContext.Registers.AddObject(register);
+            }
+        }
+
+        public void UpdateRegister(Register currentRegister)
+        {
+            this.ObjectContext.Registers.AttachAsModified(currentRegister, this.ChangeSet.GetOriginal(currentRegister));
+        }
+
+        public void DeleteRegister(Register register)
+        {
+            if ((register.EntityState != EntityState.Detached))
+            {
+                this.ObjectContext.ObjectStateManager.ChangeObjectState(register, EntityState.Deleted);
+            }
+            else
+            {
+                this.ObjectContext.Registers.Attach(register);
+                this.ObjectContext.Registers.DeleteObject(register);
+            }
+        }
+    }
+}
+
+
---
+++ domain-service-duplex-by-auto-refresh-demo/domain-service-duplex-by-auto-refresh-demo/MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.ServiceModel.DomainServices.Client;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using domain_service_duplex_by_auto_refresh_demo.Web;
+
+namespace domain_service_duplex_by_auto_refresh_demo
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            guardDataSource.RefreshInterval = TimeSpan.FromSeconds(5);
+            officerDataSource.RefreshInterval = TimeSpan.FromSeconds(5);
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            Register register = new Register {name = nameTextBox.Text, status = "Wait"};
+            RegisterDomainContext context = guardDataSource.DomainContext as RegisterDomainContext;
+            context.Registers.Add(register);
+            guardDataSource.SubmitChanges();
+        }
+
+        private void AcceptButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            Register register = button.DataContext as Register;
+            register.status = "Accept";
+            officerDataSource.SubmitChanges();
+        }
+
+        private void GuardDataSource_OnLoadingData(object sender, LoadingDataEventArgs e)
+        {
+            e.LoadBehavior = LoadBehavior.MergeIntoCurrent;
+        }
+    }
+}
