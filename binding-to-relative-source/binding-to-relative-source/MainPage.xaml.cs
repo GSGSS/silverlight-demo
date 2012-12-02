d183639c0ee9d77393f6ed7a850a54fa337d28ea
kiwi <kiwi.swhite.coder@gmail.com>
2012-12-02 15:04:38 +0800

    binding-to-relative-source example

---
+++ binding-to-relative-source/binding-to-relative-source/MainPage.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace binding_to_relative_source
+{
+    public partial class MainPage : UserControl, INotifyPropertyChanged 
+    {
+        public bool State { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            var kiwis = new List<Kiwi>();
+            kiwis.Add(new Kiwi {Name = "a"});
+            kiwis.Add(new Kiwi {Name = "b"});
+            kiwis.Add(new Kiwi {Name = "c"});
+            kiwis.Add(new Kiwi {Name = "d"});
+
+            kiwiDataGrid.ItemsSource = kiwis;
+
+            State = true;
+        }
+
+        private void ChangeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            State = !State;
+            NotifyPropertyChanged("State");
+        }
+    }
+
+    public class Kiwi
+    {
+        public String Name { get; set; }
+    }
+}
---
+++ binding-to-relative-source/binding-to-relative-source/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("binding_to_relative_source")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("binding_to_relative_source")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f6a34e5-3caa-4823-9089-2075cb9d9f88")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
