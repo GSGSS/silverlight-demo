f58a61e172314e81c6d10e79c9a7278b0b2ec3e3
kiwi <kiwi.swhite.coder@gmail.com>
2013-01-10 14:30:39 +0800

    datagrid-radiobutton-demo solve radiobutton or checkbox page display problem

---
+++ datagrid-raidobutton-demo/datagrid-raidobutton-demo/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Controls;
+
+namespace datagrid_raidobutton_demo
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
+namespace datagrid_raidobutton_demo.Web
+{
+    public partial class Book
+    {
+        private Boolean _isSelected;
+
+        public Boolean IsSelected
+        {
+            get
+            {
+                if (_isSelected == null) _isSelected = false;
+                return _isSelected;
+            }
+            set { _isSelected = value; }
+        }
+    }
+}
